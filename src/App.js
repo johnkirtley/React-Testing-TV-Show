867fbf9315fc18b2264b31fd514d945612de2b0a
johnkirtley <kirtleyj16@gmail.com>
2020-02-24 16:03:49 -0800

    Getting application to render correctly

--- src/App.js
+++ src/App.js
@@ -1,31 +1,30 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
 import Dropdown from "react-dropdown";
 import parse from "html-react-parser";
 
 import { formatSeasons } from "./utils/formatSeasons";
+import fetchShow from './api/fetchShow';
 
 import Episodes from "./components/Episodes";
 import "./styles.css";
 
 export default function App() {
   const [show, setShow] = useState(null);
-  const [seasons, setSeasons] = useState(null);
-  const [selectedSeason, setSelectedSeason] = useState("");
+  const [seasons, setSeasons] = useState([]);
+  const [selectedSeason, setSelectedSeason] = useState('');
   const episodes = seasons[selectedSeason] || [];
 
+  const getData = () => {
+    fetchShow()
+      .then(res => {
+        console.log(res)
+        setShow(res);
+        setSeasons(formatSeasons(res._embedded.episodes))
+      })
+  }
+
   useEffect(() => {
-    const fetchShow = () => {
-      axios
-        .get(
-          "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
-        )
-        .then(res => {
-          setShow(res.data);
-          setSeasons(formatSeasons(res.data._embedded.episodes));
-        });
-    };
-    fetchShow();
+    getData()
   }, []);
 
   const handleSelect = e => {
---
+++ src/App.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from './App';
+
+
+test('App renders without crashing', () => {
+    const { } = render(<App />)
+})
